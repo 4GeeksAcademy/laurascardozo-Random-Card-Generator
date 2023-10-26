384b43250732df6027ecc7d8d0dd57bf3bd389d3
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2023-10-26 20:47:41 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomFigure());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomFigure = function() {
+  let figures = ["spade", "diamond", "club", "heart"];
+  let randomFigure = Math.floor(Math.random() * figures.length);
+  return figures[randomFigure];
+};
+
+let generateRandomNumber = function() {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumber];
 };

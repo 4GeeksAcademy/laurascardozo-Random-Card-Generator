e6ac9d061b876b64e5e2cbe0f42e3008d1ea9caa
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2023-11-02 21:33:46 +0000

    funciones optimizadas

--- src/app.js
+++ src/app.js
@@ -5,19 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
   document.querySelector(".card").classList.add(generateRandomFigure());
   document.querySelector(".card").innerHTML = generateRandomNumber();
 };
 
-let generateRandomFigure = function() {
+let generateRandomFigure = () => {
   let figures = ["spade", "diamond", "club", "heart"];
   let randomFigure = Math.floor(Math.random() * figures.length);
   return figures[randomFigure];
 };
 
-let generateRandomNumber = function() {
+let generateRandomNumber = () => {
   let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
   let randomNumber = Math.floor(Math.random() * numbers.length);
   return numbers[randomNumber];
